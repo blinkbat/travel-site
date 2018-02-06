179af36f45b897dba9fec385f5f368078c02078b
David <david.bennett@ncf.edu>
2018-02-06 14:29:56 -0500

    Finished header

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,11 +1,30 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
 	constructor() {
 		this.siteHeader = $(".site-header");
+
 		this.headerTrigger = $(".large-hero__subtitle");
+
+		// call method on page load
 		this.createHeaderWaypoint();
+
+		this.pageSections = $(".page-section");
+
+		this.createPageSectionWaypoints();
+
+		// smooth scroll broken?
+		//this.addSmoothScroll();
+
+		// grab all links in nav
+		this.headerLinks = $(".primary-nav a");
+	}
+
+	addSmoothScroll() {
+		this.headerLinks.smoothScroll();
+
 	}
 
 	createHeaderWaypoint() {
@@ -21,6 +40,50 @@ class StickyHeader {
 					that.siteHeader.removeClass("site-header--dark");
 				}
 			}
+		});
+	}
+
+	createPageSectionWaypoints() {
+		var that = this;
+
+		this.pageSections.each(function() {
+			
+			var currentPageSection = this;
+			
+			new Waypoint({
+				element: currentPageSection,
+
+				handler: function(direction) {
+					if (direction == "down") {
+						var matchingLink = currentPageSection.getAttribute("data-matching-link");
+
+						that.headerLinks.removeClass("is-current-link");
+
+						$(matchingLink).addClass("is-current-link");
+					}
+				},
+
+				offset: "18%"
+
+			});
+
+			new Waypoint({
+				element: currentPageSection,
+
+				handler: function(direction) {
+					if (direction == "up") {
+						var matchingLink = currentPageSection.getAttribute("data-matching-link");
+
+						that.headerLinks.removeClass("is-current-link");
+
+						$(matchingLink).addClass("is-current-link");
+					}
+				},
+
+				offset: "-40%"
+				
+			});
+
 		});
 	}
 }
